4ac9c7308444bd852949841b598cd95730381ee4
M. Noerder-Tuitje <marcus.noerder-tuitje@crowdcode.io>
2019-05-03 09:33:32 +0200

    NCX-7 - added basic test

--- src/main/java/io/crowdcode/bgav/Plugin.java
+++ src/main/java/io/crowdcode/bgav/Plugin.java
@@ -1,10 +1,5 @@
 package io.crowdcode.bgav;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Scm;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
@@ -12,10 +7,16 @@ import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -30,17 +31,48 @@ public class Plugin extends AbstractMojo {
   private MavenProject mavenProject;
   private Scm scm;
 
+  final Log log = getLog();
+
+  /**
+   * TODO: document me
+   *
+   * @throws MojoExecutionException
+   * @throws MojoFailureException
+   */
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    getLog().info("Hello, world.");
-    getLog().info(mavenProject.toString());
-    getLog().info("getArtifactId " + mavenProject.getArtifactId());
-    getLog().info("getVersion    " + mavenProject.getVersion());
+
+    log.info("Hello, world.");
+    log.info(mavenProject.toString());
+    log.info("getArtifactId " + mavenProject.getArtifactId());
+    log.info("getVersion    " + mavenProject.getVersion());
     // Ticket-ID schon vorhanden
     mavenProject.setVersion(mavenProject.getVersion() + "-NCX-1-SNAPSHOT");
-    getLog().info("getVersion    " + mavenProject.getVersion());
+    log.info("getVersion    " + mavenProject.getVersion());
 
     File pomfile = new File("pom.xml");
+
+    Model model = getModel(pomfile);
+
+    log.info("model    " + model);
+    log.info("model    " + model.getVersion());
+    model.setVersion(model.getVersion() + "-NCX-1-SNAPSHOT");
+    try {
+      new MavenXpp3Writer().write(new FileOutputStream(pomfile), model);
+    } catch (IOException ex) {
+      log.error("IOException: " + ex);
+      throw new MojoExecutionException("could not write POM: " + ex);
+    }
+  }
+
+  /**
+   * TODO document me
+   *
+   * @param pomfile
+   * @return
+   * @throws MojoExecutionException
+   */
+  Model getModel(File pomfile) throws MojoExecutionException {
     Model model = null;
     FileReader reader = null;
     MavenXpp3Reader mavenreader = new MavenXpp3Reader();
@@ -52,15 +84,7 @@ public class Plugin extends AbstractMojo {
       getLog().error("ParseError: " + ex);
       throw new MojoExecutionException("could not read POM: " + ex);
     }
-    getLog().info("model    " + model);
-    getLog().info("model    " + model.getVersion());
-    model.setVersion(model.getVersion() + "-NCX-1-SNAPSHOT");
-    try {
-      new MavenXpp3Writer().write(new FileOutputStream(pomfile), model);
-    } catch (IOException ex) {
-      getLog().error("IOException: " + ex);
-      throw new MojoExecutionException("could not write POM: " + ex);
-    }
+    return model;
   }
 
 }
