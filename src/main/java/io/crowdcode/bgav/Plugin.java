8f0b9ff66879490d7527ded0c41844a7790a62d6
Andreas Ernst <andreas.ernst@crowdcode.io>
2019-08-05 16:50:47 +0200

    NCX-51 removed git action

--- src/main/java/io/crowdcode/bgav/Plugin.java
+++ src/main/java/io/crowdcode/bgav/Plugin.java
@@ -197,7 +197,8 @@ public class Plugin extends AbstractMojo {
             if (!nonBgavVersion.equals(model.getVersion())) {
                 log.info("none BGAV - set correct none branched version to: " + nonBgavVersion);
                 new XMLHandler(log).writeChangedPomWithXPath(pomfile, nonBgavVersion);
-                gitHandler.commitAndPush(git, nonBgavVersion + " - none BGAV - set correct none branched version");
+//                gitHandler.commitAndPush(git, nonBgavVersion + " - none BGAV - set correct none branched version");
+                log.info("none BGAV - set correct none branched version to: " + nonBgavVersion);
 //                throw new MojoExecutionException("build failed due to new none branched version, new version pushed and committed.");
             } else {
                 log.info("no BGAV information inside POM Version.");
